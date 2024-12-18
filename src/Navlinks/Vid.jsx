84c1b2be2399cfd097623481ad1676dcff001a08
Mscode04 <ms.shaheenkp@gmail.com>
2024-12-18 17:00:28 +0530

    updated

--- src/Navlinks/Vid.jsx
+++ src/Navlinks/Vid.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import '../App.css';
 
-
 function Vid() {
   return (
     <div className="vid-page">
-      <h1 style={{color:'white'}} >Video Tutorial</h1>
+      <h1 style={{ color: 'white' }}>Video Tutorial</h1>
       <div className="video-container">
         <iframe
-          width="500px"
-          height="300px"
-          src="https://www.youtube.com/embed/_VaOX6jRD3Y"  // Replace with actual video ID
+          src="https://www.youtube.com/embed/_VaOX6jRD3Y"
           title="YouTube video tutorial"
           frameBorder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
-        <p>In this video tutorial, we explain how to analyze Instagram profiles effectively. Watch the video for a step-by-step guide.</p>
-      
+        <p>
+          In this video tutorial, we explain how to analyze Instagram profiles
+          effectively. Watch the video for a step-by-step guide.
+        </p>
       </div>
     </div>
   );
