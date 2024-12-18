955c94861692eaebdc9f102f416ccf35e32859d2
Mscode04 <ms.shaheenkp@gmail.com>
2024-12-18 16:43:43 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+
+import { Routes, Route,Link  } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Vid from './Navlinks/Vid';
 import About from './Navlinks/About';
@@ -9,24 +9,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Dashbord from './components/Dashbord';
 import './App.css';
 
+
 function App() {
-  const [buttonText, setButtonText] = useState("Let's Start");
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const handleButtonClick = () => {
-    if (buttonText === "Reopen Website") {
-      // Redirect the user to the dashboard page by reloading the base URL
-      window.location.href = "https://mscode04.github.io/FAYLO/#/";
-    } else {
-      // Navigate to the dashboard for the first time
-      navigate('/');
-      setButtonText("Reopen Website"); // Change button text to 'Reopen Website'
-    }
-  };
-
-  // Only show the button on the home page ('/') or dashboard ('/dashboard')
-  const showButton = location.pathname === '/' || location.pathname === '/dashboard';
 
   return (
     <div className="app">
@@ -34,18 +18,21 @@ function App() {
         <Navbar />
         <div className="side-and-body">
           <div className="main-body">
-            {showButton && (
+           
               <div className="container start">
-                {/* Button to navigate and change text */}
-                <button onClick={handleButtonClick}>{buttonText}</button>
+                
+                <Link className="nav-link" to="/">
+                <button >Lets Start</button>
+                              </Link>
               </div>
-            )}
+            
             <Routes>
               <Route path="/tutorials" element={<Vid />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
               <Route path="/doc" element={<Doc />} />
               <Route path="/" element={<Dashbord />} />
+            
             </Routes>
           </div>
         </div>
