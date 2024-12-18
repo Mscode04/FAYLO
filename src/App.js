eaae6e91476d4324ce54a32a9ad005efd9d117a6
Mscode04 <ms.shaheenkp@gmail.com>
2024-12-18 16:13:51 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState,  } from 'react';
-import { Routes, Route,  useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Vid from './Navlinks/Vid';
 import About from './Navlinks/About';
@@ -10,8 +10,9 @@ import Dashbord from './components/Dashbord';
 import './App.css';
 
 function App() {
-  const [buttonText, setButtonText] = useState("Lets Start");
+  const [buttonText, setButtonText] = useState("Let's Start");
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleButtonClick = () => {
     if (buttonText === "Refresh") {
@@ -24,16 +25,21 @@ function App() {
     }
   };
 
+  // Only show the button on the home page ('/') or dashboard ('/dashboard')
+  const showButton = location.pathname === '/' || location.pathname === '/dashboard' ||location.pathname === '/FAYLO';
+
   return (
     <div className="app">
-      <div className='nav-bar'>
+      <div className="nav-bar">
         <Navbar />
         <div className="side-and-body">
-          <div className='main-body'>
-            <div className='container start'>
-              {/* Button to navigate and change text */}
-              <button onClick={handleButtonClick}>{buttonText}</button>
-            </div>
+          <div className="main-body">
+            {showButton && (
+              <div className="container start">
+                {/* Button to navigate and change text */}
+                <button onClick={handleButtonClick}>{buttonText}</button>
+              </div>
+            )}
             <Routes>
               <Route path="/tutorials" element={<Vid />} />
               <Route path="/about" element={<About />} />
